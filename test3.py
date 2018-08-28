4acc44581cda4fad1d435f7297999b31fa35b7a8
liyao379178 <muzi379178228@qq.com>
2018-08-28 14:54:19 +0800

    修改错误账号截图

--- test3.py
+++ test3.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 import random
 import time
 import pymysql
 import re
-import xlrd
 
 
 class New():
 	driver = webdriver.Chrome()
-	driver.maximize_window()
+	# driver.maximize_window()
 	f = open("C:\\Users\\user\\PycharmProjects\\test_find\\账号.txt","w")
-	for i in range(2):	# 修改range里的值，可以注册对应的账号
-		user = "1470000" + ''.join(random.sample("1234567890", 4))  # 随机user手机号
+	for i in range(1):	# 修改range里的值，可以注册对应的账号
+
+		user = "1470000" + ''.join(random.sample("1234567890",4))  # 随机user手机号
 		url = "https://uathome.uuabc.com/Public/newlogin/index.html#/register"
 		driver.get(url)
 		time.sleep(1)
@@ -19,15 +20,16 @@ class New():
 		driver.find_element_by_id("phone").send_keys(user)  # 传入user手机号
 		time.sleep(1)
 		driver.find_element_by_class_name("badge").click()
-		time.sleep(1)
+
+		time.sleep(2)
 		driver.find_element_by_id("code").clear()
 		"""
 		数据库查询user对应的短信
 		"""
-		conn = pymysql.connect(host="xxxx",
-							   user="xxxx",
-							   password="xxxxx",
-							   db="xxxxx",
+		conn = pymysql.connect(host="uathome.uuabc.com",
+							   user="wufenfen",
+							   password="wufenfen",
+							   db="newuuabc",
 							   port=3306,
 							   charset="utf8"
 							   )
@@ -41,6 +43,8 @@ class New():
 		cur.close()
 
 		r = re.findall('\d{6}', str(re_dock))  # 正则获取短信验证码
+		driver.find_element_by_id("code").clear()
+		time.sleep(1)
 		driver.find_element_by_id("code").send_keys(r)
 		time.sleep(1)
 		driver.find_element_by_id("name").clear()
@@ -51,9 +55,24 @@ class New():
 		driver.find_element_by_id("pwd").send_keys(123456)
 		time.sleep(1)
 		driver.find_element_by_css_selector("#app > div > div.wrapper > div > div.main > a").click()
+		time.sleep(2)
+		next_one = driver.find_element_by_class_name("skipBtn")
+
+		# 点击下一步判断，如注册失败后，截图存入image
+		if next_one== None:
+			driver.save_screenshot("C:\\Users\\user\\PycharmProjects\\test_find\\image\\" + user + ".png")
+			driver.close()
+		else:
+			f.write(user + "\n")
 		time.sleep(1)
-		f.write(user+"\n")
-		print(user)
+
+		cookies = driver.get_cookies()
+		time.sleep(1)
+		# print(f"main: cookies = {cookies}")
+		driver.delete_all_cookies()
+		time.sleep(2)
+
 	f.write("\n"+"注册完成的账号，每次执行后上一次数据会被替换掉")
 	f.close()
 	driver.close()
+
