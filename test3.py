2c479b47ac882b18de283c28fa27c12cb8069a7e
liyao379178 <muzi379178228@qq.com>
2018-08-29 12:10:38 +0800

    不启动浏览器模式下进行自动注册

--- test3.py
+++ test3.py
@@ -4,13 +4,16 @@ import random
 import time
 import pymysql
 import re
+from selenium.webdriver.chrome.options import Options
 
 
 class New():
-	driver = webdriver.Chrome()
+	chrome_options = Options()
+	chrome_options.add_argument("--headless")
+	driver = webdriver.Chrome(executable_path=(r"C:\Users\user\Downloads\chromedriver_win32\chromedriver.exe"), chrome_options=chrome_options)
 	# driver.maximize_window()
 	f = open("C:\\Users\\user\\PycharmProjects\\test_find\\账号.txt","w")
-	for i in range(1):	# 修改range里的值，可以注册对应的账号
+	for i in range(3):	# 修改range里的值，可以注册对应的账号
 
 		user = "1470000" + ''.join(random.sample("1234567890",4))  # 随机user手机号
 		url = "https://uathome.uuabc.com/Public/newlogin/index.html#/register"
@@ -44,35 +47,37 @@ class New():
 
 		r = re.findall('\d{6}', str(re_dock))  # 正则获取短信验证码
 		driver.find_element_by_id("code").clear()
-		time.sleep(1)
+		# time.sleep(1)
 		driver.find_element_by_id("code").send_keys(r)
 		time.sleep(1)
 		driver.find_element_by_id("name").clear()
 		name = "测试" + ''.join(random.sample("你真的很逗比", 2))  # 随机用户name
 		driver.find_element_by_id("name").send_keys(name)
-		time.sleep(1)
+		# time.sleep(1)
 		driver.find_element_by_id("pwd").clear()
 		driver.find_element_by_id("pwd").send_keys(123456)
 		time.sleep(1)
 		driver.find_element_by_css_selector("#app > div > div.wrapper > div > div.main > a").click()
 		time.sleep(2)
-		next_one = driver.find_element_by_class_name("skipBtn")
-
-		# 点击下一步判断，如注册失败后，截图存入image
-		if next_one== None:
-			driver.save_screenshot("C:\\Users\\user\\PycharmProjects\\test_find\\image\\" + user + ".png")
-			driver.close()
-		else:
-			f.write(user + "\n")
-		time.sleep(1)
+		# next_one = driver.find_element_by_class_name("skipBtn")
+		#
+		# # 点击下一步判断，如注册失败后，截图存入image
+		# if next_one== None:
+		# 	driver.save_screenshot("C:\\Users\\user\\PycharmProjects\\test_find\\image\\" + user + ".png")
+		# 	driver.close()
+		# else:
+		#
+		# time.sleep(1)
 
 		cookies = driver.get_cookies()
 		time.sleep(1)
 		# print(f"main: cookies = {cookies}")
 		driver.delete_all_cookies()
-		time.sleep(2)
+		time.sleep(1)
+		print(user)
+		f.write(user + "\n")
 
 	f.write("\n"+"注册完成的账号，每次执行后上一次数据会被替换掉")
 	f.close()
-	driver.close()
+	driver.quit()
 
